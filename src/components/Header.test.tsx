4a7b85a94a3771d0ceb260d98c61bd060f274a01
geekymon2 <geekymon2@gmail.com>
2025-08-05 20:53:23 +1000

    fixed tests

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Header from "./Header";
+import "@testing-library/jest-dom";
 
 describe("Header", () => {
   it("renders the app title", () => {
